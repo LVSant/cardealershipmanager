9f96f42ccf419bf70dff5bd85ccaacda16dcd53f
leonardo <leonardoviveirossantos@gmail.com>
2017-03-30 15:24:34 -0300

    Adicionado Launcher, ImageButton Garagem, FloatingIcon Adicionar Veiculo com busca no WS da Tabela Fipe

---
+++ app/src/main/java/com/anew/devl/cardealershipmanager/GaragemActivity.java
@@ -0,0 +1,13 @@
+package com.anew.devl.cardealershipmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GaragemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garagem);
+    }
+}
