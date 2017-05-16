2998a3bc6d8bd503ee68c14757db1148a89ac91f
leonardo <leonardoviveirossantos@gmail.com>
2017-05-16 16:33:57 -0300

    adicionado suporte para busca de motos e carros

--- app/src/main/java/com/anew/devl/cardealershipmanager/MainActivity.java
+++ app/src/main/java/com/anew/devl/cardealershipmanager/MainActivity.java
@@ -2,12 +2,12 @@ package com.anew.devl.cardealershipmanager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.anew.devl.cardealershipmanager.fipeclient.FipeSelectCarActivity;
@@ -29,13 +29,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ImageView imgCar = (ImageView) findViewById(R.id.imgviewCar);
+        imgCar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                startBuscaFipe();
+            public void onClick(View v) {
+                startBuscaFipe(true);
+            }
+        });
+
+        ImageView imgMoto = (ImageView) findViewById(R.id.imgviewMoto);
+        imgMoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBuscaFipe(false);
             }
         });
 
@@ -51,9 +57,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void startBuscaFipe() {
+    private void startBuscaFipe(boolean isCar) {
         if (Utils.isOnline(getBaseContext())) {
-            Intent in = new Intent(getBaseContext(), FipeSelectMotoActivity.class);
+            Intent in;
+            if (isCar) {
+                in = new Intent(getBaseContext(), FipeSelectCarActivity.class);
+            } else {
+                in = new Intent(getBaseContext(), FipeSelectMotoActivity.class);
+            }
             in.putExtra(FipeSelectMotoActivity.PASSO_BUSCA, "1");
             startActivity(in);
         } else {
